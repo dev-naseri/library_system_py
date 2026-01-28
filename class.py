df2d62746037c64c481241ea0f6c4d7c4a0d65c4
Hossein Naseri <dev.h.naseri@gmail.com>
2026-01-28 15:07:00 +0330

    last_change_is_here

--- class.py
+++ class.py
@@ -1,32 +1,4 @@
-from curses.ascii import EM
+from enum import Enum
 
-
-class SMSNotifire:
-    def notify(self, msg):
-        return f"SMS : {msg} send to contancts."
-
-
-class EmailNotifire:
-    def notify(self, msg):
-        return f"Email : {msg} send to users."
-
-
-class PushNotifire:
-    def notify(self, msg):
-        return f"Push : {msg} send to browswers."
-
-
-class NotificationManager:
-    def __init__(self, notifires=[]):
-        self.notifires = notifires
-
-    def send(self, msg):
-        if not self.notifires:
-            return False
-
-        for notifire in self.notifires:
-            print(notifire.notify(msg))
-
-
-n1 = NotificationManager([SMSNotifire(), EmailNotifire(), PushNotifire()])
-n1.send("Hello world")
+class JustData(Enum):
+    A = "APPLICATION"
